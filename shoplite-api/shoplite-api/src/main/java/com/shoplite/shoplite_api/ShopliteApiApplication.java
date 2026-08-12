f1f0894315fd52b9614e60e3c59182c28cd87501
Engenheiro Muniz <engenheiro.muniz@outlook.com>
2026-08-11 23:01:50 -0300

    fead: estrutura inicial do projeto Spring Boot com camadas definidas

---
+++ shoplite-api/shoplite-api/src/main/java/com/shoplite/shoplite_api/ShopliteApiApplication.java
@@ -0,0 +1,13 @@
+package com.shoplite.shoplite_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopliteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopliteApiApplication.class, args);
+	}
+
+}
---
+++ shoplite-api/shoplite-api/src/test/java/com/shoplite/shoplite_api/ShopliteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoplite.shoplite_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopliteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
