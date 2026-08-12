1926a82bcf2c1f83f7f40b6a8d747a5615c01509
Engenheiro Muniz <engenheiro.muniz@outlook.com>
2026-08-12 08:51:04 -0300

    feat: foram adicionadas as entidades JPA: Categoria, Produto, Cliente, Pedido, ItemPedido, Cupom. Também foi convertida application.propoerties para application.yml

---
+++ shoplite-api/shoplite-api/src/main/java/com/shoplite/shoplite_api/model/ItemPedido.java
@@ -0,0 +1,27 @@
+package com.shoplite.shoplite_api.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class ItemPedido {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    private Integer quantidade;
+    private BigDecimal precoUnitario; 
+}
---
+++ shoplite-api/shoplite-api/src/main/java/com/shoplite/shoplite_api/model/StatusPedido.java
@@ -0,0 +1,7 @@
+package com.shoplite.shoplite_api.model;
+
+public enum StatusPedido {
+    ABERTO,
+    CONFIRMADO,
+    CANCELADO
+}
